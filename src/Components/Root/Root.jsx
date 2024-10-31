4238d5bfa9c5fcd9f205effbe2a55706f1abb55d
SaikatAhmed78 <shaikatahmed78@gmail.com>
2024-11-01 03:49:48 +0600

    Display Book Card and Responsive Card Layout

--- src/Components/Root/Root.jsx
+++ src/Components/Root/Root.jsx
@@ -5,7 +5,7 @@ import { Outlet } from 'react-router-dom';
 
 const Root = () => {
     return (
-        <div className='w-11/12 mx-auto'>
+        <div className='max-w-6xl mx-auto'>
           <Navbar></Navbar> 
           <Outlet></Outlet>
           <Footer></Footer>
